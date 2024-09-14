c5908cddaa360c4f735e5fb34451ed77d1e9b4ee
hari <harikrishnap4b9@gmail.com>
2024-09-14 16:54:22 +0530

    Values Modified In Class

--- src/TestB.java
+++ src/TestB.java
@@ -2,12 +2,12 @@
 public class TestB {
 
 	public void Bemo() {
-		int a = 10,b=20;
+		int a = 5,b=10;
 		int c = a+b;
 		System.out.println(c);
 	}
 	public void division() {
-		int a = 40,b=20;
+		int a = 400,b=20;
 		int c = a/b;
 		System.out.println(c);
 	}
