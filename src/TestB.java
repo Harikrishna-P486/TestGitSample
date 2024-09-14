8896b0bc2fb240d1127a133bafcea3c1a2fedc30
hari <harikrishnap4b9@gmail.com>
2024-09-14 16:51:52 +0530

    New Class Created

---
+++ src/TestB.java
@@ -0,0 +1,14 @@
+
+public class TestB {
+
+	public void Bemo() {
+		int a = 10,b=20;
+		int c = a+b;
+		System.out.println(c);
+	}
+	public void division() {
+		int a = 40,b=20;
+		int c = a/b;
+		System.out.println(c);
+	}
+}
